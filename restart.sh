3378363bc98c05907b3ffc20f32f71688a9a5fc8
Nick LaPrell <nick@laprell.org>
2016-12-01 23:13:00 +0000

    Updating bash scripts to share a common log

--- restart.sh
+++ restart.sh
@@ -6,4 +6,4 @@ forever stop --sourceDir $APPROOT --workingDir $APPROOT --workingDir $PWD -t $AP
 rm $APPROOT/logs/*.log
 forever start -a --sourceDir $APPROOT --workingDir $APPROOT -l $APPROOT"/logs/log.log" -e $APPROOT"/logs/error.log" server.js
 forever start -a --sourceDir $APPROOT --workingDir $APPROOT -l $APPROOT"/logs/log.log" -e $APPROOT"/logs/error.log" wemo.js
-forever start -a --sourceDir $APPROOT --workingDir $APPROOT -l $APPROOT"/logs/jobs.log" -e $APPROOT"/logs/error.log" jobs.js
+forever start -a --sourceDir $APPROOT --workingDir $APPROOT -l $APPROOT"/logs/log.log" -e $APPROOT"/logs/error.log" jobs.js
