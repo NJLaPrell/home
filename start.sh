c0732cd75d30732970ea0fc330798ee1f444fc66
Nick LaPrell <nick@laprell.org>
2016-12-01 22:22:11 +0000

    Just updating the bash scripts for the new application home.

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-APPROOT="/home/pi/webapps/test/home"
+APPROOT="/home/pi/webapps/home"
 rm $APPROOT/logs/*.log
 forever start -a --sourceDir $APPROOT --workingDir $APPROOT -l $APPROOT"/logs/log.log" -e $APPROOT"/logs/error.log" server.js
 forever start -a --sourceDir $APPROOT --workingDir $APPROOT -l $APPROOT"/logs/log.log" -e $APPROOT"/logs/error.log" wemo.js
--- stop.sh
+++ stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-APPROOT="/home/pi/webapps/test/home"
+APPROOT="/home/pi/webapps/home"
 forever stop --sourceDir $APPROOT --workingDir $APPROOT --workingDir $PWD -t $APPROOT"/server.js"
 forever stop --sourceDir $APPROOT --workingDir $APPROOT --workingDir $PWD -t $APPROOT"/wemo.js"
 forever stop --sourceDir $APPROOT --workingDir $APPROOT --workingDir $PWD -t $APPROOT"/jobs.js"
