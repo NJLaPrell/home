31882cc11ba9ee543371ab7a447a5218816ffbe2
Nick LaPrell <nick@laprell.org>
2016-12-07 11:47:13 -0700

    Updated to better find the script source.

--- helpers/log.js
+++ helpers/log.js
@@ -1,12 +1,19 @@
-var stackTrace = require('stack-trace');
-
 module.exports = {
 	getScript: function(script){
-		if(!script){
-			return stackTrace.get()[2].getFileName();
-		} else {
-			return script;
-		}
+		var originalFunc = Error.prepareStackTrace;
+	    var callerfile;
+	    try {
+	        var err = new Error();
+	        var currentfile;
+	        Error.prepareStackTrace = function (err, stack) { return stack; };
+	        currentfile = err.stack.shift().getFileName();
+	        while (err.stack.length) {
+	            callerfile = err.stack.shift().getFileName();
+	            if(currentfile !== callerfile) break;
+	        }
+	    } catch (e) {}
+	    Error.prepareStackTrace = originalFunc; 
+	    return callerfile;
 	},
 	log: function(msg, type, script){
 		script = this.getScript(script);
