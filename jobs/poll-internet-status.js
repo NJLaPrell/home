9a98e62e149999c2bfe8a866db3a48e67718b9d7
Nick LaPrell <nick@laprell.org>
2016-12-09 13:57:11 -0700

    Converting to the trigger-event helper.

--- jobs/poll-internet-status.js
+++ jobs/poll-internet-status.js
@@ -3,6 +3,7 @@ var request = require('request');
 var roku = require('../helpers/roku');
 var log = require('../helpers/log.js');
 var ping = require('ping');
+var triggerEvent = require('../helpers/trigger-event.js');
 
 // Log the started schedule
 log.startup("Job Scheduled");
@@ -11,37 +12,8 @@ log.startup("Job Scheduled");
 module.exports = {
 	schedule: '*/1 * * * *', 
 	job: function(){
-
-		var ops = {
-			host: "www.google.com",
-			timeout: 1000,
-			method: "GET",
-			path: "/"
-		};
-		var http = require('http');
-		
-		var triggerEvent = function(result){
-			var conf = require('../config.js'); 
-			var payload = {
-				overrideUser: conf.uName,
-				overridePassword: conf.password,
-				event: "internetCheck",
-				args: {
-					pass: result
-				}
-			};
-			request.post(
-			    'http://localhost/home/trigger-event',
-			    {json: payload},
-			    function (error, response, body) {
-			        if (error) {
-			            log.error("Unable to trigger ineternetCheck event: " + error);
-			        } 
-			    }
-			);
-		};
 		ping.sys.probe("8.8.8.8", function(isAlive){
-			triggerEvent(isAlive);
+			triggerEvent('internetCheck', {pass:isAlive});
 		});
 	}
 };
