712b0b06e65cdd159baf9a2f42cf7c91d919a3e2
Nick LaPrell <nick@laprell.org>
2016-12-09 09:26:47 -0700

    Updating the listener to call the panic Hue Animation function when the internet first goes offline.

--- listeners/internet-check.js
+++ listeners/internet-check.js
@@ -1,11 +1,21 @@
+var HueAnimation = require('../helpers/hue-animation.js');
+
 module.exports = function(house){
 	house.listenForEvent('internetCheck', function(args){
 		house.recordTriggeredListener('internetCheck');
+
+		// Set the internetAccess status variable
 		house.setStatus('internetAccess', args.pass);
+
+		// The internet just came back up
 		if(house.getStatus('internetDownSince') && args.pass){
 			house.setStatus('internetDownSince', null);
+			
+		// The internet just went down
 		} else if(house.getStatus('internetDownSince') == null && args.fail){
 			house.setStatus('internetDownSince', date.toString());
+			var animation = new HueAnimation();
+			animation.trigger("panic");
 		}	
 	});
 };
