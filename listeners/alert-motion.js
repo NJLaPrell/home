10d20f24061e8e9ab6662206715ae8be779fcdd7
Nick LaPrell <nick@laprell.org>
2016-12-11 13:51:50 -0700

    Adding Brenda's location.

--- listeners/alert-motion.js
+++ listeners/alert-motion.js
@@ -5,7 +5,7 @@ module.exports = function(house){
 		house.recordTriggeredListener('email-received');
 		var date = new Date();
 		house.setStatus('motionLastDetected', date.toString());
-		if(args.subject.indexOf('Alarm:Cam_495920') != -1 && house.getStatus('nickslocation') == 'away'){
+		if(args.subject.indexOf('Alarm:Cam_495920') != -1 && house.getStatus('nickslocation') == 'away' && house.getStatus('brendaslocation') == 'away'){
 			alert("Motion detected on Camera 495920");
 		}		
 	});
