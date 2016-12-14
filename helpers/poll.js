2ab8fc9d063c8251aee9fdba899e7a122035a2f8
Nick LaPrell <nick@laprell.org>
2016-12-14 16:44:50 -0700

    Created a poll class for creating polling jobs. Updated server.js to invoke them and copied weather over.

---
+++ helpers/poll.js
@@ -0,0 +1,48 @@
+
+function Poll(settings){
+	this.triggerEvent = require('./trigger-event.js');;
+	this.conf = require('../config.js');;
+	this.log = require('./log.js');;
+
+	this.name = settings.name;
+	this.intervalString = settings.interval;
+	this.executeOnStartup = settings.executeOnStartup;
+	this.job = null;
+
+	var parts = settings.interval.split(" ");
+	if(parts.length != 2){
+		this.log.error("Invalid timer format. Received: " + settings.interval);
+		return false;
+	}
+	/*
+	if(!parseInt(parts[0] > 0)){
+		this.log.error("BInvalid timer format. Received: " + settings.interval);
+		return false;
+	}
+	*/
+	var multiplyer = parts[1] == 's' ? 1000 : parts[1] == 'm' ? 60000 : parts[1] == 'h' ? 3600000 : false;
+	if(!multiplyer){
+		this.log.error("Invalid timer format. Received: " + settings.interval);
+		return false;
+	}
+	this.interval = parts[0] * multiplyer;
+	return this;
+}
+
+Poll.prototype.setJob = function(job){
+	this.job = job;
+};
+
+Poll.prototype.execute = function(){
+	this.log.startup("Polling scheduled for " + this.name + " to trigger every " + this.intervalString);
+
+	setInterval(this.job, this.interval);
+
+	if(this.executeOnStartup){
+		this.job();
+	}
+};
+
+
+
+module.exports = Poll;
---
+++ polls/poll-weather.js
@@ -0,0 +1,40 @@
+// Imported Modules
+var http = require('http');
+var Poll = require('../helpers/poll.js');
+
+var settings = {
+	name: 'Weather',
+	interval: '1 m',
+	executeOnStartup: true
+};
+
+var poll = new Poll(settings);
+
+poll.setJob(function(){
+	var url = 'http://api.openweathermap.org/data/2.5/weather?id=5318313&APPID=' + this.conf.weatherAPIKey + '&units=imperial';
+	var self = this;
+	http.get(url, function(res){
+	    var body = '';
+
+	    res.on('data', function(chunk){
+	        body += chunk;
+	    });
+
+	    res.on('end', function(){
+	        var response = JSON.parse(body);
+	        var args = {
+	        	description: response.weather[0].main,
+	        	temp: Math.round(response.main.temp),
+	        	humidity: response.main.humidity,
+	        	wind: Math.round(response.wind.speed),
+	        	clouds: response.clouds.all,
+	        	icon: response.weather[0].icon
+	        };
+	        self.triggerEvent('weather-status', args);
+	    });
+	}).on('error', function(e){
+	      console.log("Got an error: ", e);
+	});	
+});
+
+module.exports = poll;
