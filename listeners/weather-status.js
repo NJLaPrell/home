5ca07c65dd77b6be87488861ec5f816fb11e4ae2
Nick LaPrell <nick@laprell.org>
2016-12-15 08:48:37 -0700

    Updated listener to calculate.

--- listeners/weather-status.js
+++ listeners/weather-status.js
@@ -2,5 +2,18 @@ module.exports = function(house){
 	house.listenForEvent('weather-status', function(args){
 		house.recordTriggeredListener('weather-status');
 		house.setStatus('currentWeather', args);
+		
+		// Day/Night not yet set
+		if(!house.getStatus('daytime') && !house.getStatus('nighttime')){
+			var date = new Date();
+			var time = date.getTime()/1000;
+			if(time < args.sunrise || time > args.sunset){
+				house.setStatus('daytime', false);
+				house.setStatus('nighttime', true);
+			} else {
+				house.setStatus('daytime', true);
+				house.setStatus('nighttime', false);
+			}
+		}
 	});
 };
