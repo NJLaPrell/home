8cff47e5fe5ba2212c9bb3bdce527bddf63322ae
Nick LaPrell <nick@laprell.org>
2016-12-15 13:14:24 -0700

    Updating startup scripts to no longer call the jobs in a second thread.

--- stop.sh
+++ stop.sh
@@ -2,4 +2,3 @@
 APPROOT="/home/pi/webapps/home"
 forever stop --sourceDir $APPROOT --workingDir $APPROOT --workingDir $PWD -t $APPROOT"/server.js"
 forever stop --sourceDir $APPROOT --workingDir $APPROOT --workingDir $PWD -t $APPROOT"/wemo.js"
-forever stop --sourceDir $APPROOT --workingDir $APPROOT --workingDir $PWD -t $APPROOT"/jobs.js"
