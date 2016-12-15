3a0fc3be8b12b3a79f8b1de33d90a1630d658a57
Nick LaPrell <nick@laprell.org>
2016-12-15 08:21:47 -0700

    Moving wake roku logic into the listener.

--- listeners/tv-check.js
+++ listeners/tv-check.js
@@ -1,6 +1,15 @@
+var roku = require('../helpers/roku'); 
+
 module.exports = function(house){
 	house.listenForEvent('tv-status', function(args){
 		house.recordTriggeredListener('tv-status');
 		house.setStatus('tvStatus', args.status);
+		if(args.status == 'off'){
+			roku.executeCommand("wake", function(response){
+				if(response.status < 200 || response.status <= 300){
+					house.log.warning("Found Roku powered off. Can't wake it. He's dead, Jim.");
+				}	
+			});
+		}
 	});
 };
