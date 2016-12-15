bf8391b7b33a5f4290dd4f5dd597f4c5d36d4823
Nick LaPrell <nick@laprell.org>
2016-12-15 08:15:58 -0700

    Moving UPS Status.

--- listeners/ups-status.js
+++ listeners/ups-status.js
@@ -2,5 +2,10 @@ module.exports = function(house){
 	house.listenForEvent('ups-status', function(args){
 		house.recordTriggeredListener('ups-status');
 		house.setStatus('upsStatus', args);
+		// Set the house to powered if the UPS reports online (as opposed to ONBATT)
+		// This is used for initial state on server start.
+		if(args.status == 'ONLINE' && !house.getStatus('powered')){
+			house.setStatus('powered', true);
+		}
 	});
 };
