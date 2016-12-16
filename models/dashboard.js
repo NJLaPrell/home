a21169257dbba171559b3b47c3370291d59da6dd
Nick LaPrell <nick@laprell.org>
2016-12-16 08:32:46 -0700

    Fixing up readability

--- models/dashboard.js
+++ models/dashboard.js
@@ -4,26 +4,30 @@ var fs = require('fs');
 module.exports = function(house){
 	var model = {}
 	model.status = {};
+
 	model.status.serverTime = date.getDateTime();
+
+	// Weather Information
 	model.status.daytime = house.status.daytime 
 	model.status.nighttime = house.status.nighttime;
+	model.status.currentWeather = house.status.currentWeather;
+
+	// Location Information
 	model.status.nickIsHome = house.status.nickslocation == 'home' ? true : false;
 	model.status.nickIsAway = house.status.nickslocation == 'away' ? true : false;
 	model.status.nickIsUnknown = house.status.nickslocation ? false : true;
 	model.status.brendaIsHome = house.status.brendaslocation == 'home' ? true : false;
 	model.status.brendaIsAway = house.status.brendaslocation == 'away' ? true : false;
 	model.status.brendaIsUnknown = house.status.brendaslocation ? false : true;
+
+	// Camera Information
 	model.status.motionLastDetected = house.status.motionLastDetected
 	model.status.motionDetected = house.status.motionLastDetected ? true : false;
+	model.status.motionWhileAway = house.status.motionWhileAway;
+
+	// Power Information
 	model.status.powered = house.status.powered;
-	model.status.powerOutSince = house.status.powerOutSince;
-	model.status.internet = house.status.internetAccess;
-	model.status.internetOutSince = house.status.internetOutSince;
-	model.status.tv = {
-		on: (house.status.tvStatus == 'on' ? true : false),
-		of: (house.status.tvStatus == 'off' ? true : false),
-		sleeping: (house.status.tvStatus == 'sleeping' ? true : false)
-	};
+	model.status.powerOutSince = house.status.powerOutSince;	
 	model.status.ups = {};
 	model.status.ups.status = house.status.upsStatus ? house.status.upsStatus.status : 'Unknown';
 	model.status.ups.startTime = house.status.upsStatus ? house.status.upsStatus.startTime : 'Unknown';
@@ -34,20 +38,34 @@ module.exports = function(house){
 	model.status.ups.timeLeft = house.status.upsStatus ? house.status.upsStatus.timeLeft : 'Unknown';
 	model.status.ups.batteryVoltage = house.status.upsStatus ? house.status.upsStatus.batteryVoltage : 'Unknown';
 	model.status.ups.timeOnBattery = house.status.upsStatus ? house.status.upsStatus.timeOnBattery : 'Unknown';
-	model.status.currentWeather = house.status.currentWeather;
+	
+	// Internet Information
+	model.status.internet = house.status.internetAccess;
+	model.status.internetOutSince = house.status.internetOutSince;
+	
+	// TV Information
+	model.status.tv = {
+		on: (house.status.tvStatus == 'on' ? true : false),
+		of: (house.status.tvStatus == 'off' ? true : false),
+		sleeping: (house.status.tvStatus == 'sleeping' ? true : false)
+	};
+	
+	// Debug Information
 	model.debug = {
 		listenersTriggered: house.listenersTriggered,
 		listenersRegistered: house.listenersRegistered,
 		eventsFired: house.eventsFired
 	};
-
-	model.status.plugs = house.status.plugs;
-
 	model.errorLog  = fs.readFileSync(__dirname + '/../logs/log.log');
 
-	model.status.eventHistory = house.status.eventHistory;
+	// Edimax Smart Plug Information
+	model.status.plugs = house.status.plugs;
 
-	model.status.motionWhileAway = house.status.motionWhileAway;
+	// Hue Light Information
+	
+
+	// History Information
+	model.status.eventHistory = house.status.eventHistory;
 
 	return model;
 }
