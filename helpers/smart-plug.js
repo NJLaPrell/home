4d4f9024d0daf0e3157537a4113ff610f94767a5
Nick LaPrell <nick@laprell.org>
2016-12-19 11:15:39 -0700

    Preventing logging for switches that have not been discovered.

--- helpers/smart-plug.js
+++ helpers/smart-plug.js
@@ -27,9 +27,7 @@ module.exports = {
 		if(typeof state === 'undefined'){
 			var self = this;
 			state = this.getState(name).then(function(state){
-				smartplug.setSwitchState(!state, self.getOptions(name)).catch(function(e) {
-					log.warning("Failed to get the state of switch: " + name);
-				});
+				smartplug.setSwitchState(!state, self.getOptions(name));
 			});
 
 		} else {
