068a0c4b17a050d224c1ed7609fd65d7cfe399e5
Nick LaPrell <nick@laprell.org>
2016-12-21 15:49:53 -0700

    Creating new poll with ping utility

---
+++ polls/person-location.js
@@ -0,0 +1,38 @@
+// Imported Modules
+var Poll = require('../helpers/poll.js');
+var ping = require('ping');
+
+var settings = {
+	name: 'Person Location',
+	interval: '30 s',
+	executeOnStartup: true
+};
+
+var poll = new Poll(settings);
+
+poll.setJob(function(house){
+	var self = this;
+	ping.sys.probe(house.conf.nicksPhoneIP, function(isAlive){
+		if(!house.getStatus('nickslocation')){
+			house.setStatus('nickslocation', (isAlive ? 'home' : 'away'));
+		}
+		if(isAlive && house.getStatus('nickslocation') == 'away'){
+			self.triggerEvent('gps', {person:'nick', location: 'home'});
+		} else if(!isAlive && house.getStatus('nickslocation') == 'home'){
+			self.triggerEvent('gps', {person:'nick', location: 'away'});
+		}
+	}, {timeout: 10});
+
+	ping.sys.probe(house.conf.brendasPhoneIP, function(isAlive){
+		if(!house.getStatus('brendalocation')){
+			house.setStatus('brendalocation', (isAlive ? 'home' : 'away'));
+		}
+		if(isAlive && house.getStatus('brendalocation') == 'away'){
+			self.triggerEvent('gps', {person:'brenda', location: 'home'});
+		} else if(!isAlive && house.getStatus('brendalocation') == 'home'){
+			self.triggerEvent('gps', {person:'brenda', location: 'away'});
+		}
+	}, {timeout: 10});
+});
+
+module.exports = poll;
