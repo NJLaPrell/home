f0f6671e67484b09c3b0e5ccab8bacd59919f072
Nick LaPrell <nick@laprell.org>
2016-12-29 07:11:55 -0700

    Fixing response status check.

--- components/listeners/tv-check.js
+++ components/listeners/tv-check.js
@@ -15,7 +15,7 @@ listener.setListener(function(house){
 		house.setStatus('tvStatus', args.status);
 		if(args.status == 'off'){
 			roku.executeCommand("wake", function(response){
-				if(response.status < 200 || response.status <= 300){
+				if(response.status < 200 || response.status >= 300){
 					house.log.warning("Found Roku powered off. Can't wake it. He's dead, Jim.\rResponse: " + JSON.stringify(response));
 				}	
 			});
