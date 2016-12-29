9deed06b07ae61998a2efa682568f41260e22002
Nick LaPrell <nick@laprell.org>
2016-12-29 07:14:19 -0700

    Fixed bug where house class was being called out of scope.

--- helpers/text-alert.js
+++ helpers/text-alert.js
@@ -4,7 +4,7 @@ var Log = require('./log.js');
 var log = new Log(conf.debug);
 
 module.exports = function(msg){	
-	house.log.debug("Sending text alert: " + msg);
+	log.debug("Sending text alert: " + msg);
 	var message = 'From: LaPrell Home <' + conf.textAlert.smtpEnvelope.from + '>\r\n';
 	message	+= 'To: IFTTT <' + conf.textAlert.smtpEnvelope.to + '>\r\n';
 	message	+= 'Subject: ' + conf.textAlert.subject + '\r\n';
