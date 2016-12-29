1b762b0d707490915a826dcbccf94b727d5eca4f
Nick LaPrell <nick@laprell.org>
2016-12-29 16:14:10 -0700

    Fixing the listener to check for a valid email subject to preven throwing an error.

--- components/listeners/alert-motion.js
+++ components/listeners/alert-motion.js
@@ -14,21 +14,23 @@ var listener = new Listener(settings);
 listener.setListener(function(house){
 	house.listenForEvent('email-received', function(args){
 		house.recordTriggeredListener('email-received');
-		house.setStatus('motionLastDetected', date.getDateTime());
+		if(args.subject){
+			house.setStatus('motionLastDetected', date.getDateTime());
 
-		var away = house.getStatus('nickslocation') == 'away' && house.getStatus('brendaslocation') == 'away' ? true : false;
-		var motion = args.subject.indexOf('Alarm:Cam_495920') != -1 ? true : false;
-		if(motion && away){
-			house.log.debug("Motion detected while everyone was away.");
-			if(!house.getStatus('motionWhileAway')){
-				alert("Motion detected on Camera 495920");
-				house.logHistory("Motion was detected when nobody was home.");	
-				house.triggerEvent('motion-while-away');
-			}
-			house.setStatus('motionWhileAway', true);
-		} else if(motion && !away){
-			house.setStatus('motionWhileAway', false);
-		}	
+			var away = house.getStatus('nickslocation') == 'away' && house.getStatus('brendaslocation') == 'away' ? true : false;
+			var motion = args.subject.indexOf('Alarm:Cam_495920') != -1 ? true : false;
+			if(motion && away){
+				house.log.debug("Motion detected while everyone was away.");
+				if(!house.getStatus('motionWhileAway')){
+					alert("Motion detected on Camera 495920");
+					house.logHistory("Motion was detected when nobody was home.");	
+					house.triggerEvent('motion-while-away');
+				}
+				house.setStatus('motionWhileAway', true);
+			} else if(motion && !away){
+				house.setStatus('motionWhileAway', false);
+			}	
+		}
 	});
 });
 
