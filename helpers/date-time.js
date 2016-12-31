ae88749ce5438f038f149a20e964541721c9402a
Nick LaPrell <nick@laprell.org>
2016-12-31 10:54:19 -0700

    Moving to the moment.js library for time formatting.

--- helpers/date-time.js
+++ helpers/date-time.js
@@ -1,29 +1,13 @@
+var moment = require('moment');
+
 module.exports = {
 	getDate: function(){
-		var date = new Date();
-		var year = date.getFullYear().toString();
-		var month = (date.getMonth()+1).toString();
-		month = month < 10 ? '0' + month : month;
-		var day = date.getDate().toString();
-		day = day < 10 ? '0' + day : day;
-		return year + '-' + month + '-' + day;
+		return moment().format("YYYY-MM-DD");
 	},
 	getTime: function(){
-		var date = new Date();
-		var hours = date.getHours();
-		var ampm = 'AM';
-		if(hours > 12){
-			ampm = 'PM';
-			hours = hours - 12;
-		}
-		hours = hours < 10 ? '0' + hours : hours;
-		var minutes = date.getMinutes();
-		minutes = minutes < 10 ? '0' + minutes : minutes;
-		var seconds = date.getSeconds();
-		seconds = seconds < 10 ? '0' + seconds : seconds;
-		return hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+		return moment().format("hh:mm:ss A");
 	},
 	getDateTime: function(){
-		return this.getDate() + ' ' + this.getTime();
+		return moment().format("YYYY-MM-DD hh:mm:ss A");
 	}
 };
