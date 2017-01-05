5647d8b7094cf0abb452ea967a9eb9455f9f9d39
Nick LaPrell <nick@laprell.org>
2017-01-05 07:15:48 -0700

    Fixing bad reference to the lutron service.

--- components/listeners/trigger-lutron.js
+++ components/listeners/trigger-lutron.js
@@ -14,7 +14,7 @@ var listener = new Listener(settings);
 listener.registerListener('trigger-lutron', function(house, args){
 	house.recordTriggeredListener('trigger-lutron');
 	var brightness = args.direction ? (args.direction == 'on' ? 100 : 0) : args.brightness;
-	house.lutron.setBrightness(args.light, brightness);		
+	house.servicesRegistered['Lutron Caseta Hub'].lutron.setBrightness(args.light, brightness);		
 });
 
 module.exports = listener;
